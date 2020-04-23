229eeb5946c027b4272e1890585c0c71451f17d0
DC <43692686+excaliburDC@users.noreply.github.com>
2020-04-23 21:06:06 +0530

    created navmesh for level 1

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="AI/Create Enemy")]
+public class Enemy : ScriptableObject
+{
+    public GameObject enemyPrefab;
+    public float moveSpeed;
+    public float health;
+    public float attackPower;
+    public float attackRate;
+    public float attackRange;
+    public float selfDamagePower;
+    public Animator enemyAnim;
+}
