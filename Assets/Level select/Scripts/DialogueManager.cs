1ae550797abb2e24ad3a47f042b2dd7dd9762a5f
meghapandichelvam <57277665+meghapandichelvam@users.noreply.github.com>
2020-04-24 10:29:20 +0530

    Scripts for the dialogue System

---
+++ Assets/Level select/Scripts/DialogueManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour
+{
+    public Text speakerText;
+    public Text dialogueText;
+    public Animator Anim;
+    private Queue<string> sentences;
+
+     void Start()
+    {
+        sentences = new Queue<string>();
+    }
+   
+    public void StartDialogue(Dialogue dialogue)
+    {
+        // Debug.Log("Start converstion with " + dialogue.name);
+        Anim.SetBool("IsOpen", true);
+        speakerText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+        
+    }
+    void EndDialogue()
+    {
+        Anim.SetBool("IsOpen", false);
+        Debug.Log("End of dialogue");
+    }
+}
---
+++ Assets/Level select/Scripts/Register.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Register : MonoBehaviour
+{
+
+    public GameObject userName;
+    public GameObject email;
+    public GameObject password;
+    public GameObject confirmPassword;
+
+    private string UserName;
+    private string Email;
+    private string Password;
+    private string ConfirmPassword;
+    private string form;
+
+    private bool EmailValid = false;
+
+   
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void RegisterDialogue()
+    {
+        bool UN = false;
+        bool EM = false;
+        bool PW = false;
+        bool CPW = false;
+
+        if (UserName != "" &&  Email != "" && Password != "" && ConfirmPassword != "")
+        {
+            Debug.Log("Registeration success");
+            if (UserName != "")
+            {
+                if (System.IO.File.Exists(@"E:\TestFiles/" + UserName + ".txt"))
+                {
+                    UN = true;
+                }
+                else
+                {
+                    Debug.LogWarning("Username Taken");
+                }
+
+            }else
+            {
+                Debug.LogWarning("Username field empty");
+            }
+            if(Email != "")
+            {
+                EmailValidation();
+                if (EmailValid)
+                {
+                    if(Email.Contains("@"))
+                    {
+                        if(Email.Contains("."))
+                        {
+                            EM = true;
+                        }else
+                        {
+                            Debug.LogWarning("Email is incorrect");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Email is incorrect");
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning("Email is incorrect");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Email field Empty");
+            }
+            if(Password != "")
+            {
+                if(Password.Length >5)
+                {
+                    PW = true;
+                }else
+                {
+                    Debug.LogWarning("Password must be atleast 6 character long");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Password Field is Empty");
+            }
+            if(ConfirmPassword != "")
+            {
+                if(ConfirmPassword == Password)
+                {
+                    CPW = true;
+                }else
+                {
+                    Debug.LogWarning("Password Dont match");
+                }
+            }else
+            {
+                Debug.LogWarning("Confirm Password Field is Empty");
+            }
+
+            if(UN == true && EM == true && PW ==true && CPW == true)
+            {
+                bool Clear = true;
+                int i = 1;
+                foreach(Char c in Password)
+                {
+                    if(Clear)
+                    {
+                        Password = "";
+                        Clear = false;
+                    }
+                    i++;
+                    Char Encrypted = (char)(c * i);
+                    Password += Encrypted.ToString();
+                }
+                form = (UserName + "\n" + Email + "\n" + Password);
+                System.IO.File.WriteAllText(@"E:\TestFiles/" + UserName + ".txt",form);
+                userName.GetComponent<InputField>().text = "";
+                email.GetComponent<InputField>().text = "";
+                password.GetComponent<InputField>().text= "";
+                confirmPassword.GetComponent<InputField>().text= "";
+                Debug.Log("Registeration completed");
+            }
+        }
+    }
+
+    void EmailValidation()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            if(userName.GetComponent<InputField>().isFocused)
+            {
+                email.GetComponent<InputField>().Select();
+            }
+            if (email.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+            if (password.GetComponent<InputField>().isFocused)
+            {
+                confirmPassword.GetComponent<InputField>().Select();
+            }
+        }
+
+       
+        UserName = userName.GetComponent<InputField>().text;
+        Email = email.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        ConfirmPassword = confirmPassword.GetComponent<InputField>().text;
+
+    }
+}
