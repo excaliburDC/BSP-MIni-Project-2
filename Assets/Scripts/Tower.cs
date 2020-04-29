a12b4ea70fb2095ffc49ccc1554cb54935d9d11e
DC <43692686+excaliburDC@users.noreply.github.com>
2020-04-29 14:18:27 +0530

    Started implementing towers

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Player/ Create Tower")]
+public class Tower : ScriptableObject
+{
+    public string towerName;
+    public float towerRange;
+    public float rotateSpeed;
+    public float attackPower;
+    public float attackRate;
+}
---
+++ Assets/Scripts/TowerShooter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShooter : MonoBehaviour
+{
+    public Transform target;
+    public float range = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
